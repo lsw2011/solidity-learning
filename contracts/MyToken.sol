357f1f7ac6406525ac7d1575e4ab5986c5267cd1
lsw2011 <lsw952179@gmail.com>
2026-04-04 19:18:20 +0900

    mint function

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,4 +1,4 @@
-// SPDX-Licesne-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
 contract MyToken {
@@ -13,17 +13,13 @@ contract MyToken {
         name = _name;
         symbol = _symbol;
         decimals = _decimal;
+        // transaction
+        // from, to, data, value, gas,...
+        _mint(1 * 10 ** uint256(decimals), msg.sender); // 1 MT
     }
 
-    // function totalSupply() external view returns (uint256) {
-    //     return totalSupply;
-    // }
-
-    // function balanceOf(address owner) external view returns (uint256) {
-    //     return balanceOf[owner];
-    // }
-
-    // function name() external view returns (string memory) {
-    //     return name;
-    // }
+    function _mint(uint256 amount, address owner) internal {
+        totalSupply += amount;
+        balanceOf[owner] = balanceOf[owner] + amount;
+    }
 }
