3060df8129103da31de446ec0510093b1f99c283
lsw2011 <lsw952179@gmail.com>
2026-04-11 15:16:22 +0900

    add approve transferfrom approval event and test

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -3,12 +3,15 @@ pragma solidity ^0.8.28;
 
 contract MyToken {
     event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed spender, uint256 amount);
+
     string public name;
     string public symbol;
     uint8 public decimals;
 
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) allowance;
 
     constructor(
         string memory _name,
@@ -20,6 +23,20 @@ contract MyToken {
         symbol = _symbol;
         decimals = _decimal;
         _mint(_amount * 10 ** uint256(decimals), msg.sender); // 1 MT
+    }
+
+    function approve(address spender, uint256 amount) external {
+        allowance[msg.sender][spender] = amount;
+        emit Approval(spender, amount);
+    }
+
+    function transferFrom(address from, address to, uint256 amount) external {
+        address spender = msg.sender;
+        require(allowance[from][spender] >= amount, "insufficient allowance");
+        allowance[from][spender] -= amount;
+        balanceOf[from] -= amount;
+        balanceOf[to] += amount;
+        emit Transfer(from, to, amount);
     }
 
     function _mint(uint256 amount, address owner) internal {
