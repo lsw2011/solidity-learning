86db9ce8678c72c900ebab17bc6d2b0d42ee70f4
lsw2011 <lsw952179@gmail.com>
2026-05-01 14:41:49 +0900

    tiny bank service

---
+++ contracts/TinyBank.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+// stacking
+// deposit(MyToken) / withdraw(MyTOken)
+
+interface IMyToken {
+    function transfer(uint256 amount, address to) external;
+
+    function transferFrom(address from, address to, uint256 amount) external;
+}
+
+contract TinyBank {
+    event Staked(address, uint256);
+
+    IMyToken public stackingToken;
+    mapping(address => uint256) public staked;
+    uint256 public totalStaked;
+
+    constructor(IMyToken _stackingToken) {
+        stackingToken = _stackingToken;
+    }
+
+    function stake(uint256 _amount) external {
+        require(_amount >= 0, "cannot stake 0 amount");
+        stackingToken.transferFrom(msg.sender, address(this), _amount);
+        staked[msg.sender] += _amount;
+        totalStaked += _amount;
+        emit Staked(msg.sender, _amount);
+    }
+}
